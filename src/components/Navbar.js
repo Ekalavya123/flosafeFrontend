30752527630034764181d3ca1c43ca6b5ca8a6d9
Ekalavya L <86137780+Ekalavya123@users.noreply.github.com>
2024-02-06 00:44:26 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 
 export default function Navbar() {
-  const [request,setRequest] =useState("")
   const navigate= useNavigate();
   const handleLogout=()=>{
     localStorage.removeItem('token')
     localStorage.removeItem('tokenActivate')
     navigate("/Login")
   }
-  const handleProfile=()=>{navigate("/About")}
-  const handleOnclick=()=>{
-    // navigate("/".concat("",request));
-    navigate("/")
-  }
-  let p=<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-          <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-        </svg>
-  let options=['Sites','Site1','Site2'];
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-success">
     <div class="container-fluid">
@@ -38,7 +28,7 @@ export default function Navbar() {
               <a class="nav-link dropdown-toggle mt-1 text-black" style={{width:'50%',paddingLeft:'15px'}} to="" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Sites
               </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <ul class="dropdown-menu bg-success" aria-labelledby="navbarDropdown">
                 <li><a class="dropdown-item" to="">Site1</a></li>
                 <li><a class="dropdown-item" to="">Site2</a></li>
               </ul>
