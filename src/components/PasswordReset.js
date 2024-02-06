61d17fc6ce62f177460773d479cd6962155f104d
Ekalavya L <86137780+Ekalavya123@users.noreply.github.com>
2024-02-06 11:52:37 +0530

    Update PasswordReset.js

--- src/components/PasswordReset.js
+++ src/components/PasswordReset.js
@@ -2,14 +2,16 @@ import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 export default function PasswordReset() {
     const [credentials,setCredentials]=useState({password:"",confirmPassword:""})
+    const [loading,setLoading] =useState(0)
     let authToken=localStorage.getItem("resetToken"),navigate=useNavigate();
     const setPassword = async () => {
         if(credentials.password!=credentials.confirmPassword){alert("confirmPassword is not matching");return;}
-        await fetch("https://flosafeanalyticsbackend.onrender.com/api/getUserDetails", {
+        setLoading(1)
+        try {
+            await fetch("https://flosafeanalyticsbackend.onrender.com/api/getUserDetails", {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body:JSON.stringify({authToken:authToken})
-        }).then(async (res) => {
+            body:JSON.stringify({authToken:authToken})}).then(async (res) => {
             let response= await res.json()
             //console.log("res in rest" ,response.data.email)
             if(response.success){
@@ -19,12 +21,19 @@ export default function PasswordReset() {
                 body:JSON.stringify({email:response.data.email,password:credentials.password})
                 }).then(response => response.json()).then(json => {
                     if(json.success) localStorage.removeItem('resetToken')
+                    setLoading(0);
                     alert(json.message);
                     navigate('/Login')
-                })   
+                }) 
             }
-            else alert(response.message)
-        })
+            else {setLoading(0);alert(response.message);}
+            })
+            
+        } catch (error) {
+            setLoading(0)
+            alert("Somthing went wrong try later")
+        }
+        
     } 
     const onChange=(event)=>{
         setCredentials({...credentials,[event.target.name]:event.target.value})
@@ -47,9 +56,10 @@ export default function PasswordReset() {
             <span className="fas fa-key"></span>
             <input type="password"  id="pwd" placeholder="confirmPassword" name='confirmPassword' value={credentials.confirmPassword} onChange={onChange} />
         </div>
-        <button className="btn mt-3 bg-success" onClick={setPassword}>update</button>
+        <button className="btn mt-3 bg-success" style={{width:'70%'}} onClick={setPassword}>update</button>
+        {loading?<div class="spinner-border" role="status">
+          <span class="visually-hidden text-center">Loading...</span>
+        </div>:""}
       </div>
   )
 }
-
-
