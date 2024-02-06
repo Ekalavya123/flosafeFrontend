eb65641e1534a2315ff2363d51ce7294d4bdd6bb
Ekalavya L <86137780+Ekalavya123@users.noreply.github.com>
2024-02-07 01:58:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Login from './screens/Login';
 import About from './components/About';
 import Verification from './components/Verification'
 import PasswordReset from './components/PasswordReset'
-let token=localStorage.getItem('token'),resetToken=localStorage.getItem('resetToken')
 function App() {
   return (
     <Router>
@@ -16,8 +15,8 @@ function App() {
           <Route exact path="/SignUp" element={<Signup />}/>
           <Route exact path="/Login" element={<Login />}/>
           <Route exact path="/About" element={<About />}/>
-          <Route exact path={"/users/verify/"+token} element={<Verification />}/>
-          <Route exact path={"/users/reset/"+resetToken} element={<PasswordReset />}/>
+          <Route exact path={"/users/verify/:token"} element={<Verification />}/>
+          <Route exact path={"/users/reset/:token"} element={<PasswordReset />}/>
         </Routes>
       </div>
     </Router>
